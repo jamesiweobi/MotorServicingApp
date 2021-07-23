304d9840f46ee946298c7922b31db0959d5398e8
Tobi Ajetomobi <oluwatobiajetomobi@hotmail.com>
2021-07-23 12:59:58 -0400

    navbar
    navbar

---
+++ front_end/src/components/MenuItems.js
@@ -0,0 +1,20 @@
+export const MenuItems = [
+    {
+        title: 'Service All',
+        path: '/servce-all',
+        cName: 'dropdown-link'
+    },
+
+    {
+        title: 'Our Team',
+        path: '/our-team',
+        cName: 'dropdown-link'
+    },
+
+    {
+        title: 'Pricing',
+        path: '/pricing',
+        cName: 'dropdown-link'
+    },
+
+]
