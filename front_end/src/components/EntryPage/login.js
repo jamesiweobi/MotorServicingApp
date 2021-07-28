cd5eb5aca6958f00d083c44b04ee4197299ba497
Abdullah Shehu <55896361+Abdul-Asa@users.noreply.github.com>
2021-07-28 19:58:07 +0100

    Update login.js

--- front_end/src/components/EntryPage/login.js
+++ front_end/src/components/EntryPage/login.js
@@ -5,7 +5,7 @@ import Input from './Input'
 import InputGroup from './InputGroup'
 import { EntryPage, PageHeader } from './entryPage'
 import { Link } from 'react-router-dom'
-import axios from 'axios'
+// import axios from 'axios'
 // import {useDispatch} from 'react-redux'
 // import './css/entryPage.css'
 
