b7ff1509adf0d4b132e8fd34981e42c64fb8d082
Abdullah Shehu <55896361+Abdul-Asa@users.noreply.github.com>
2021-07-28 16:59:02 +0100

    Show profile in navbar

--- front_end/src/components/Navbar/Navbar.js
+++ front_end/src/components/Navbar/Navbar.js
@@ -3,20 +3,27 @@ import { Button } from './Button';
 import { Link } from 'react-router-dom';
 import './css/Navbar.css';
 import Dropdown from './Dropdown';
+import ProfileCard from './ProfileCard';
 
 function Navbar() {
+
+const userProf = {
+    userName : "Abdullah",
+    isLoggedin : true
+}
+
 const [click, setClick] = useState(false);
 const [dropdown, setDropdown] = useState(false);
 
 const handleClick = () => setClick(!click);
 const closeMobileMenu = () => setClick(false);
 
-    const onMouseEnter = () => {
-        if (window.innerWidth < 960) {
-        setDropdown(false);
-        } else {
-        setDropdown(true);
-        }
+const onMouseEnter = () => {
+    if (window.innerWidth < 960) {
+    setDropdown(false);
+    } else {
+    setDropdown(true);
+    }
 };
 
 const onMouseLeave = () => {
@@ -77,18 +84,53 @@ const onMouseLeave = () => {
             </Link>
             </li>
 
-            <li className='nav-links-mobile'>
-            <Link
-                to ='/login'
-                className = "login-links"
-                onClick={closeMobileMenu}
-            >
-                Login
-            </Link>
-            </li>
+            {!userProf.isLoggedIn && 
+                <>
+                    <li className='nav-links-mobile'>
+                        <Link
+                            to ='/login'
+                            className = "login-links"
+                            onClick={closeMobileMenu}
+                        >
+                            Manage profile
+                        </Link>
+                    </li>  
+                    <li className='nav-links-mobile'>
+                        <Link
+                            to ='/login'
+                            className = "login-links"
+                            onClick={closeMobileMenu}
+                        >
+                            My cart
+                        </Link>
+                    </li>   
+                    <li className='nav-links-mobile'>
+                        <Link
+                            to ='/login'
+                            className = "login-links"
+                            onClick={closeMobileMenu}
+                        >
+                            Logout
+                        </Link>
+                    </li>   
+                </> 
+            }
 
+            {userProf.isLoggedIn && 
+                <li className='nav-links-mobile'>
+                    <Link
+                        to ='/login'
+                        className = "login-links"
+                        onClick={closeMobileMenu}
+                    >
+                        Login
+                    </Link>
+                </li>   
+            }
         </ul>
-        <Button text="Login"/>
+
+        {userProf.isLoggedIn && <Button text="Login"/>}
+        {!userProf.isLoggedIn && <ProfileCard userN={userProf.userName}/> }
     </nav>
     </>
 );
