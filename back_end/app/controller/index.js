12f1cf3f40847827fa32d303e9335894e229add1
jamesiweobi <60060852+jamesiweobi@users.noreply.github.com>
2021-07-30 19:59:15 +0100

    changed error status code and fixed failing test bug

--- back_end/app/controller/index.js
+++ back_end/app/controller/index.js
@@ -1,15 +1,15 @@
 const { signupValidation, loginValidation } = require('../validation');
 const User = require('../models');
 const bcrypt = require('bcryptjs');
-const jwt = require("jsonwebtoken");
-const _ = require("lodash");
-const dotenv = require("dotenv");
-const mailgun = require("mailgun-js");
+const jwt = require('jsonwebtoken');
+const _ = require('lodash');
+const dotenv = require('dotenv');
+const mailgun = require('mailgun-js');
 
 dotenv.config();
 
 const DOMAIN = process.env.DOMAIN;
-const mg = mailgun({apiKey: process.env.MAILGUN_APIKEY, domain: DOMAIN});
+const mg = mailgun({ apiKey: process.env.MAILGUN_APIKEY, domain: DOMAIN });
 
 const signup = async (req, res) => {
   const emailExist = await User.findOne({ email: req.body.email });
@@ -32,14 +32,15 @@ const signup = async (req, res) => {
         error: 'Failed to create new User',
         message: error.details[0].message,
       });
+
+    } else {
+      await newUser.save();
     }
-      
-    await newUser.save();
-      
     return res.status(200).json({
-        message: 'Created new User',
-        user: newUser,
-      });
+      message: 'Created new User',
+      user: newUser,
+    });
+
   } catch (error) {
     throw error;
   }
@@ -71,76 +72,86 @@ const login = async (req, res) => {
 };
 
 const forgotPassword = (req, res) => {
-    const {email} = req.body;
+  const { email } = req.body;
 
-    User.findOne({email}, (err, user) => {
-        if(err || !user) {
-            return res.status(400).json({error: "User with this email does not exist."});
-        }
+  User.findOne({ email }, (err, user) => {
+    if (err || !user) {
+      return res
+        .status(400)
+        .json({ error: 'User with this email does not exist.' });
+    }
 
-        const token = jwt.sign(
-            {_id: user._id},
-            process.env.RESET_PASSWORD_KEY,
-            {
-                expiresIn: "5h",
-            }
-        );
-        const data = {
-            from: 'noreply@thepowerteam.com',
-            to: email,
-            subject: 'Password reset link',
-            html: `<h2>Please click on given link to reset password</h2>
-                    <p>${process.env.CLIENT_URI}/resetpassword/${token}</p>`
-        };
-
-        return user.updateOne({resetLink: token}, function(err, success) {
-            if(err) {
-            return res.status(400).json({error: "Reset password link error."});
-            } else {
-            mg.messages().send(data, function (error, body) {
-                if(error) {
-                    return res.json({error: err});
-                }
-                return res.json({message: "Email has been sent. Kindly follow the instructions"});
-            });
-            }
-        });
+    const token = jwt.sign({ _id: user._id }, process.env.RESET_PASSWORD_KEY, {
+      expiresIn: '5h',
     });
-}
+    const data = {
+      from: 'noreply@thepowerteam.com',
+      to: email,
+      subject: 'Password reset link',
+      html: `<h2>Please click on given link to reset password</h2>
+                    <p>${process.env.CLIENT_URI}/resetpassword/${token}</p>`,
+    };
+
+    return user.updateOne({ resetLink: token }, function (err, success) {
+      if (err) {
+        return res.status(400).json({ error: 'Reset password link error.' });
+      } else {
+        mg.messages().send(data, function (error, body) {
+          if (error) {
+            return res.json({ error: err });
+          }
+          return res.json({
+            message: 'Email has been sent. Kindly follow the instructions',
+          });
+        });
+      }
+    });
+  });
+};
 
 const resetPassword = (req, res) => {
-    const {resetLink, newPassword} = req.body;
-    if(resetLink) {
-        jwt.verify(resetLink, process.env.RESET_PASSWORD_KEY, function(error, decodedData) {
-            if(error) {
-            return res.status(401).json({error: "Incorrect token or it is expired."});
+  const { resetLink, newPassword } = req.body;
+  if (resetLink) {
+    jwt.verify(
+      resetLink,
+      process.env.RESET_PASSWORD_KEY,
+      function (error, decodedData) {
+        if (error) {
+          return res
+            .status(401)
+            .json({ error: 'Incorrect token or it is expired.' });
+        }
+        User.findOne({ resetLink }, (err, user) => {
+          if (err || !user) {
+            return res
+              .status(400)
+              .json({ error: 'User with this token does not exist.' });
+          }
+
+          //encrypt the reset password
+          // encryptedUserPassword = bcrypt.hash(newPassword, 10);
+
+          const obj = {
+            password: newPassword,
+            resetLink: '',
+          };
+
+          user = _.extend(user, obj);
+          user.save((err, result) => {
+            if (err) {
+              return res.status(400).json({ error: 'Reset password error.' });
+            } else {
+              return res
+                .status(200)
+                .json({ message: 'Your password has been changed' });
             }
-            User.findOne({resetLink}, (err, user) => {
-                if(err || !user) {
-                return res.status(400).json({error: "User with this token does not exist."});
-                }
-
-                //encrypt the reset password
-                // encryptedUserPassword = bcrypt.hash(newPassword, 10);
-
-                const obj = {
-                    password: newPassword,
-                    resetLink: ''
-                }
-
-                user = _.extend(user, obj);
-                user.save((err, result) => {
-                    if(err) {
-                    return res.status(400).json({error: "Reset password error."});
-                    } else {
-                        return res.status(200).json({message: "Your password has been changed"});                
-                    }
-                });
-            });
+          });
         });
-    } else {
-        return res.status(401).json({error: "Authentication error!!!."});
-    }
-}
+      }
+    );
+  } else {
+    return res.status(401).json({ error: 'Authentication error!!!.' });
+  }
+};
 
 module.exports = { signup, login, forgotPassword, resetPassword };
