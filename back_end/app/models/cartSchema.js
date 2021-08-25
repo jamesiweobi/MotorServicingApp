34dc29c69bb6fa3de6d3dafa49589769af4c1006
YemmyAde <yemisadesanya@gmail.com>
2021-08-25 19:26:09 +0100

    fixing conflicts

--- back_end/app/models/cartSchema.js
+++ back_end/app/models/cartSchema.js
@@ -5,69 +5,6 @@ const emailValidator = function (el) {
   return isEmail(el);
 };
 
-// const cartSchema = new mongoose.Schema({
-//   vehicle_details: [
-//     {
-//       make: {
-//         type: String,
-//         required: [true, 'Please enter the vehicle make'],
-//         trim: true,
-//       },
-//       year: {
-//         type: Number,
-//         required: [true, 'Please enter the vehicle year'],
-//         trim: true,
-//       },
-//       model: {
-//         type: String,
-//         required: [true, 'Please enter the vehicle year'],
-//       },
-//       millage: {
-//         type: Number,
-//         trim: true,
-//       },
-//       service_required: [
-//         {
-//           type: String,
-//           required: [true, 'Please enter the service for required'],
-//         },
-//       ],
-//     },
-//   ],
-//   appointment_date: {
-//     type: Date,
-//     required: [true, 'Please a date for your appointment'],
-//   },
-//   appointment_time: {
-//     type: Date,
-//     required: [true, 'Please a time for your appointment'],
-//   },
-//   contact_details: {
-//     customer_name: {
-//       type: String,
-//       required: [true, "Please enter customen's name for our record."],
-//       trim: true,
-//     },
-//     customer_email: {
-//       type: String,
-//       lowercase: true,
-//       validate: {
-//         validator: [emailValidator, 'Please enter a correct Email.'],
-//       },
-//     },
-//     customer_phone_number: {
-//       type: Number,
-//       min: [7, 'Phone number is too short.'],
-//       required: [true, 'Please enter a valid phone number.'],
-//     },
-//     additional_message: {
-//       type: String,
-//       maxlength: [1000, 'Message limit exceeded.'],
-//     },
-//   },
-//   cart_total: Number,
-// });
-
 const cartSchema = new mongoose.Schema({
   vehicle_details: [
     {
